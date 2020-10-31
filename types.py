816f2da1b1ee2b451b486ff92323de30a9949dba
olesialakhadanova <lesik.095@mail.ru>
2020-11-01 00:52:38 +0300

    add types numbers - string

---
+++ types.py
@@ -0,0 +1,20 @@
+# PEP-8 Style coding
+
+# number
+
+# x = 5                   # int       
+# y = 1.1                 # float
+
+# i = (2,3)               # complex
+# i = 2 + 3j              # complex
+
+# z = 2 + x * y / 10 - 100
+
+# print(z)
+# print(i)
+
+# string
+
+first_name = 'Olesia'
+
+print(first_name)
